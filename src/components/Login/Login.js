ec0ad4b07aee72facf107a63fb8a935fe669d4cf
alex.serbang@outlook.com <alex.serbang@outlook.com>
2020-05-17 23:38:42 +0300

    Final header styling

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { actions } from '../../redux/auth.redux';
+import styles from './Login.module.css';
 
 class Login extends React.Component {
   state = {
@@ -18,28 +19,40 @@ class Login extends React.Component {
   handleSubmit = () => {
     const { value } = this.state;
     const { actions, onSuccess, history } = this.props;
-    actions.signIn(value)
-      .then(onSuccess)
-      .then(() => history.push(history.location.state.from))
-      .catch(e => this.setState({
-        error: e
-      }));
+    if (value) {
+      actions.signIn(value)
+        .then(onSuccess)
+        .then(() => history.push(history.location.state.from))
+        .catch(e => this.setState({
+          error: e
+        }));
+    }
   }
 
   render() {
     const { value, error } = this.state;
     const { users } = this.props;
     return (
-      <div>
-        <select value={value} onChange={this.handleChange}>
-          <option value="" disabled>Select an user</option>
-          {users.map(({ id, name }) => <option key={id} value={id}>{name}</option>)}
-        </select>
-        <div>
-          <button onClick={this.handleSubmit}>Sign in</button>
+      <div className={styles.login}>
+        <div className={styles.title}>
+          <h4>Welcome to the Would You Rather App!</h4>
+          <p>Please sign in to continue</p>
         </div>
-        <div>
-          {error !== null && <p>Something went wrong. Please try again</p>}
+        <div className={styles.logo}>
+          <img src={require(`../../assets/images/react-redux.png`)} width={120} height={120} />
+        </div>
+        <div className={styles.selectUser}>
+          <h3 >Sign in</h3>
+          <select value={value} onChange={this.handleChange}>
+            <option value="" disabled>Select an user</option>
+            {users.map(({ id, name }) => <option key={id} value={id}>{name}</option>)}
+          </select>
+          <div className={`${styles.submit} ${value === '' ? styles.disabled : null}`} onClick={this.handleSubmit}>
+            Sign in
+          </div>
+          <div>
+            {error !== null && <p>Something went wrong. Please try again</p>}
+          </div>
         </div>
       </div>
     )
