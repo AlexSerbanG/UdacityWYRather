9b9f354379a5c3316a94c01e0f5e0fd6d09fbe0c
alex.serbang@outlook.com <alex.serbang@outlook.com>
2020-05-17 18:31:52 +0300

    Added Leaderboard

--- src/components/Leaderboard/Leaderboard.js
+++ src/components/Leaderboard/Leaderboard.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { Score } from '../Score';
+import { connect } from 'react-redux';
 
 class Leaderboard extends React.Component {
   render() {
+    const { leaderboard = [] } = this.props;
     return <div>
-      Leaderboard
+      {leaderboard.map((user) => <Score key={user.id} {...user} />)}
     </div>
   }
 }
 
-export default Leaderboard;
+const mapStateToProps = ({ users }) => {
+  return {
+    leaderboard: users.allIds.map(id => {
+      const { answers, questions, ...user } = users.byId[id];
+      return {
+        ...user,
+        created: questions.length,
+        answered: Object.keys(answers).length,
+        score: questions.length + Object.keys(answers).length,
+      };
+    })
+      .sort((a, b) => b.score - a.score)
+  }
+}
+
+export default connect(mapStateToProps)(Leaderboard);
